16e925c5769f34ccefc8f3352ff714ca098ba2b7
Moataz Mohammady <moataz@wuilt.com>
2023-04-08 14:02:28 +0200

    add 0x15-file_io

---
+++ a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void) {
+    printf("O_RDONLY & O_WRONLY is %d", O_RDONLY & O_WRONLY);
+    printf("\n=============\n");
+    return (0);
+}
